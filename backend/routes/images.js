6e0a78301515fd53301709e099dadd25c16d9966
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-11-20 12:55:10 +0100

    upload implemented

--- backend/routes/images.js
+++ backend/routes/images.js
@@ -1,7 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const db = require('../models/db');
 
+// Configurazione di Multer per gestire l'upload dei file
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Specifica la cartella di destinazione per i file caricati
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname)); // Genera un nome univoco per il file
+  },
+});
+
+const upload = multer({ storage: storage });
+
+// Endpoint per l'upload di un singolo file
+router.post('/upload', upload.single('image'), async (req, res) => {
+  try {
+    const file = req.file;
+    if (!file) {
+      return res.status(400).json({ message: 'Nessun file caricato.' });
+    }
+
+    // Salva i dettagli del file nel database, se necessario
+    const insertQuery = 'INSERT INTO files (filename, path, isLocal) VALUES (?, ?, ?)';
+    const result = await db.query(insertQuery, [file.filename, file.path, true]);
+
+    res.json({ message: 'Upload completato con successo.' });
+  } catch (error) {
+    console.error('Errore durante l\'upload:', error);
+    res.status(500).json({ message: 'Errore durante l\'upload.' });
+  }
+});
 router.get('/', async (req, res) => {
   try {
     const results = await db.query('SELECT * FROM files');
